dd7939b9a1e5976199b1e3f312da66f2481e9d9e
Victor <josvictoronofre@gmail.com>
2026-05-16 01:09:56 +0000

    Criando o arquivo do menu

---
+++ src/view/SistemaNoticias.java
@@ -0,0 +1,59 @@
+package view;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import modelo.Noticia;
+import servico.GerenciadorNoticias;
+
+public class SistemaNoticias {
+
+	public static void main(String[] args) {
+		GerenciadorNoticias gerenciador = new GerenciadorNoticias();
+		int opcao = 0;
+		Scanner scanner = new Scanner(System.in);
+		
+		//loop que controla o menu
+		while (opcao != 4) {
+			System.out.println("\n--- Menu principal ---");
+			System.out.println("1. Adicionar manual");
+			System.out.println("2. Adicionar automatico");
+			System.out.println("3. Listar");
+			System.out.println("4 Sair");
+				
+				try {
+					opcao = scanner.nextInt();
+					switch (opcao) {
+					 case 1: {
+						System.out.print("Digite o texto: ");
+						scanner.nextLine();
+						String texto = scanner.nextLine();
+						System.out.print("Digite a classificacao: ");
+						String classificacao = scanner.nextLine();
+						gerenciador.adicionar(new Noticia(texto, classificacao));
+						break;
+					 }
+					 case 2: {
+						 System.out.print("Digite o texto: ");
+						 scanner.nextLine();
+						 String texto = scanner.nextLine();
+						 gerenciador.adicionar(new Noticia(texto));
+						 break;
+					 }
+					 case 3:
+						for (Noticia noticia : gerenciador.getTodasNoticias()) {
+							System.out.println(noticia);
+						}
+						break;
+					 case 4:
+						break;
+					}
+				} catch (InputMismatchException e) {
+					System.err.println("Erro! valor inserido invalido para o campo!");
+					scanner.nextLine();
+				}
+		}
+		scanner.close();
+	}
+
+}
